9b54b7df8a4f8a52c7cf3268ac700e3b8c0a9135
Gaber139ish <166601847+Gaber139ish@users.noreply.github.com>
2024-10-09 19:22:45 -0700

    Create js_addon.cpp
    Signed-off-by: Gaber139ish <166601847+Gaber139ish@users.noreply.github.com>

---
+++ src/js_module/src/js_addon.cpp
@@ -0,0 +1,13 @@
+#include <napi.h>
+
+Napi::String HelloWorld(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    return Napi::String::New(env, "Hello from C++!");
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+    exports.Set(Napi::String::New(env, "helloWorld"), Napi::Function::New(env, HelloWorld));
+    return exports;
+}
+
+NODE_API_MODULE(addon, Init)
